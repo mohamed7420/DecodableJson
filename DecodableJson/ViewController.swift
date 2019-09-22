0518a5c3b7118066fc5ea80993a1317b991dff75
mohamedosama2074 <iosengks74@gmail.com>
2019-09-23 00:42:26 +0200

    Initial Commit

---
+++ DecodableJson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DecodableJson
+//
+//  Created by Mohamed on 9/23/19.
+//  Copyright © 2019 Mohamed74. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
